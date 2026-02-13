0613e231424d97b1b7408c1b6d8e05e3f4f05308
cesar <cesarrodas113@gmail.com>
2026-02-13 07:08:17 -0500

    subiendo repo

--- src/controllers/medico.controller.js
+++ src/controllers/medico.controller.js
@@ -1,2 +1,12 @@
-import {medico} from  "../models/medico_model.js";
+import { medicoModel } from "../models/medico_model.js";
 
+export const getMedicos = async (req, res) => {
+  try {
+    const medicos = await medicoModel.findall();
+    res.json({ medicos });
+  } catch (error) {
+    res.status(500).json({
+      error: "Error al listar los medicos",
+    });
+  }
+};
--- src/models/medico_model.js
+++ src/models/medico_model.js
@@ -2,9 +2,9 @@ import cnx from "./bdatos.js";
 
 const db = cnx.promise();
 
-export const medico = {
+export const medicoModel = {
   findall: async () => {
-    const sql = "select * from tblmedicos";
+    const sql = "select * from tblmedico";
     const [rows] = await db.query(sql);
     return rows;
   },
