a692b885c7c0b5b5206e7cce268372d4b0ed697a
Criss <cristo0595@gmail.com>
2026-02-14 10:01:05 -0500

    controller listo tipo CRUD

--- src/controllers/medicamento.controller.js
+++ src/controllers/medicamento.controller.js
@@ -1,1 +1,89 @@
 //? Controlador para los medicamentos
+
+//* primero importo los medicamentos
+
+import { medicamentoModel } from "../models/medicamento_model.js";
+
+//* luego creo la función para listar los medicamentos */
+
+export const getMedicamentos = async(req, res) => {
+    try {
+        const result = await medicamentoModel.findall();
+        res.json({ result });
+    }
+    catch (error) {
+        res.status(500).json({ error: "Error al listar los medicamentos" });
+    }
+};
+
+//*Buscar medicamento por id */
+
+export const getMedicamentoById = async (req, res) => {
+    try {
+        const result = await medicamentoModel.findById(req.params.id);
+        res.json({ result });
+
+    }
+    catch (error) {
+        res.status(500).json({
+            error: "Error al buscar medicamento"
+        });
+        
+    }
+};
+
+//* Insertar un registro de medicamentos a la base de datos
+export const createMedicamento = async (req, res) => {
+    const data = {
+        nombreMedicamento: req.body.nombreMedicamento,
+        cantidadMedicamento: req.body.cantidadMedicamento,
+        precioMedicamento: req.body.precioMedicamento,
+        fechaVencimiento: req.body.fechaVencimiento,
+        tblFormulaMedica_idFormula: req.body.tblFormulaMedica_idFormula
+    };
+
+    try {
+        const results = await medicamentoModel.create(data);
+        res.json({ results });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error al crear el medicamento"
+        });
+        console.log(error);
+    }
+};
+
+
+//* luego creo la función para eliminar un medicamento */
+
+export const deleteMedicamento = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await medicamentoModel.delete(id);
+
+        res.json({
+            mensaje: "Medicamento eliminado correctamente",
+            result,
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: error.message,
+        });
+    }
+};
+
+
+//* funcion para actualizar un medicamento */
+
+export const updateMedicamento = async (req, res) => {
+    try {
+        const results = await medicamentoModel.update(req.params.id, req.body);
+        res.json({ results });
+    }
+    catch (error) {
+        res.status(500).json({
+            error: "Error al actualizar el medicamento"
+        });
+    }
+};
--- src/routes/medicamento.routes.js
+++ src/routes/medicamento.routes.js
@@ -1,5 +1,6 @@
 //? vamos a crear los endpoints para los medicamentos
-//* importamos las rutas para gestionar los medicamentos 
+//* importamos las rutas para gestionar los medicamentos
+
 import { Router } from 'express';
 
 //*importamos la logica del controlador de medicamentos
@@ -11,4 +12,18 @@ import * as medicamentoController from '../controllers/medicamento.controller.js
 const router = Router();
 
 //* rutas del modulo rutas para listar todo
-router.get("/")
+router.get("/medicamento/listarmedicamentos", medicamentoController.getMedicamentos);
+
+//* ruta para crear un nuevo medicamento
+router.post("/medicamento/crearmedicamento", medicamentoController.createMedicamento);  
+
+//* ruta para obtener un medicamento por su id
+router.get("/medicamento/obtenermedicamento/:id", medicamentoController.getMedicamentoById);
+
+//* ruta para actualizar un medicamento por su id
+router.put("/medicamento/actualizarmedicamento/:id", medicamentoController.updateMedicamento);
+
+//* ruta para eliminar un medicamento por su id
+router.delete("/medicamento/eliminarmedicamento/:id", medicamentoController.deleteMedicamento);
+
+export default router;
