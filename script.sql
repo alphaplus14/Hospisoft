9f3ae6d7b8500775086a6187b4a2c5ad3d6813c6
Criss <cristo0595@gmail.com>
2026-02-24 11:01:28 -0500

    actualizado

--- script.sql
+++ script.sql
@@ -2,14 +2,14 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-02-2026 a las 14:14:18
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2026 at 04:28 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+00:00";a
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -18,127 +18,144 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `hospisoft`
+-- Database: `hospisoft`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblcitas`
+-- Table structure for table `citas`
 --
 
-CREATE TABLE `tblcitas` (
+CREATE TABLE `citas` (
   `idCitas` int(11) NOT NULL,
   `fechaCita` datetime NOT NULL,
   `estado` varchar(200) NOT NULL,
   `tipoSolicitudCita` varchar(200) NOT NULL,
-  `tblMedico_idMedico` int(11) NOT NULL,
-  `tblPaciente_idPaciente` int(11) NOT NULL
+  `idMedico` int(11) NOT NULL,
+  `idPaciente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblformulamedica`
+-- Table structure for table `detalleentrada`
 --
 
-CREATE TABLE `tblformulamedica` (
+CREATE TABLE `detalleentrada` (
+  `detalleEntrada` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `nombreMedicamentoEntrada` varchar(250) NOT NULL,
+  `detallesEntrada` varchar(45) NOT NULL,
+  `fechaVencimiento` date NOT NULL,
+  `Entrada_idmovimientosMedicamentos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detalleformula`
+--
+
+CREATE TABLE `detalleformula` (
+  `iddetalleformula` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `observaciones` varchar(250) DEFAULT 'tomar cantidad por dias',
+  `formula_idFormula` int(11) NOT NULL,
+  `medicamento_idMedicamento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entrada`
+--
+
+CREATE TABLE `entrada` (
+  `idmovimientosMedicamentos` int(11) NOT NULL,
+  `fechaMovimiento` date NOT NULL,
+  `tipoMovimiento` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formula`
+--
+
+CREATE TABLE `formula` (
   `idFormula` int(11) NOT NULL,
   `fechaFormula` date NOT NULL,
-  `indicacionesFormula` varchar(200) NOT NULL
+  `medico_idtblMedico` int(11) NOT NULL,
+  `paciente_idPaciente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblhistoriaclinica`
+-- Table structure for table `historiaclinica`
 --
 
-CREATE TABLE `tblhistoriaclinica` (
+CREATE TABLE `historiaclinica` (
   `idHistoriaClinica` int(11) NOT NULL,
   `fechaHistoriaClinica` date NOT NULL,
   `diagnosticoHistoriaClinica` varchar(200) NOT NULL,
   `observacionesHistoriaClinica` varchar(200) NOT NULL,
-  `tblMedico_idMedico` int(11) NOT NULL,
-  `tblPaciente_idPaciente` int(11) NOT NULL
+  `idMedico` int(11) NOT NULL,
+  `idPaciente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblhistoriaclinica_has_tblformulamedica`
+-- Table structure for table `medicamento`
 --
 
-CREATE TABLE `tblhistoriaclinica_has_tblformulamedica` (
-  `tblHistoriaClinica_idHistoriaClinica` int(11) NOT NULL,
-  `tblFormulaMedica_idFormula` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tblmedicamento`
---
-
-CREATE TABLE `tblmedicamento` (
-  `idMedicamentos` int(11) NOT NULL,
+CREATE TABLE `medicamento` (
+  `idMedicamento` int(11) NOT NULL,
   `nombreMedicamento` varchar(200) NOT NULL,
   `cantidadMedicamento` int(11) NOT NULL,
   `precioMedicamento` int(11) NOT NULL,
-  `fechaVencimiento` date NOT NULL,
-  `tblFormulaMedica_idFormula` int(11) NOT NULL
+  `Entrada_idmovimientosMedicamentos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblmedico`
+-- Table structure for table `medico`
 --
 
-CREATE TABLE `tblmedico` (
+CREATE TABLE `medico` (
   `idMedico` int(11) NOT NULL,
   `nombreMedico` varchar(200) NOT NULL,
   `especialidadMedico` varchar(200) NOT NULL,
-  `celularMedico` varchar(200) NOT NULL
+  `celularMedico` varchar(200) NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `tblmedico`
---
-
-INSERT INTO `tblmedico` (`idMedico`, `nombreMedico`, `especialidadMedico`, `celularMedico`) VALUES
-(1, 'Carlos Ramírez', 'Cardiología', '3001234567'),
-(3, 'Andrés Martínez', 'Dermatología', '3023456789'),
-(4, 'Diana Torres', 'Ginecología', '3034567890'),
-(5, 'Felipe Castro', 'Neurología', '3045678901'),
-(6, 'Mariana López', 'Medicina General', '3056789012'),
-(7, 'Julián Herrera', 'Ortopedia', '3067890123'),
-(8, 'Valentina Rojas', 'Oftalmología', '3078901234'),
-(9, 'Santiago Moreno', 'Urología', '3089012345'),
-(10, 'Camila Vargas', 'Psicología', '3090123456');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblpaciente`
+-- Table structure for table `paciente`
 --
 
-CREATE TABLE `tblpaciente` (
+CREATE TABLE `paciente` (
   `idPaciente` int(11) NOT NULL,
   `nombrePaciente` varchar(200) NOT NULL,
   `documentoPaciente` varchar(200) NOT NULL,
   `celularPaciente` varchar(200) NOT NULL,
   `emailPaciente` varchar(200) NOT NULL,
-  `fechaNacimientoPaciente` date NOT NULL
+  `fechaNacimientoPaciente` date NOT NULL,
+  `eps` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblrol`
+-- Table structure for table `rol`
 --
 
-CREATE TABLE `tblrol` (
+CREATE TABLE `rol` (
   `idRol` int(11) NOT NULL,
   `nombreRol` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -146,176 +163,198 @@ CREATE TABLE `tblrol` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tblusuario`
+-- Table structure for table `usuario`
 --
 
-CREATE TABLE `tblusuario` (
+CREATE TABLE `usuario` (
   `idUsuario` int(11) NOT NULL,
   `nombreUsuario` varchar(200) NOT NULL,
   `correoUsuario` varchar(200) NOT NULL,
   `passwordUsuario` varchar(200) NOT NULL,
-  `tblRol_idRol` int(11) NOT NULL
+  `Rol_idRol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `tblcitas`
+-- Indexes for table `citas`
 --
-ALTER TABLE `tblcitas`
+ALTER TABLE `citas`
   ADD PRIMARY KEY (`idCitas`),
-  ADD KEY `fk_tblCitas_tblMedico1_idx` (`tblMedico_idMedico`),
-  ADD KEY `fk_tblCitas_tblPaciente1_idx` (`tblPaciente_idPaciente`);
+  ADD KEY `fk_tblCitas_tblMedico1_idx` (`idMedico`),
+  ADD KEY `fk_tblCitas_tblPaciente1_idx` (`idPaciente`);
 
 --
--- Indices de la tabla `tblformulamedica`
+-- Indexes for table `detalleentrada`
 --
-ALTER TABLE `tblformulamedica`
-  ADD PRIMARY KEY (`idFormula`);
+ALTER TABLE `detalleentrada`
+  ADD PRIMARY KEY (`detalleEntrada`),
+  ADD KEY `fk_detalleEntrada_Entrada1_idx` (`Entrada_idmovimientosMedicamentos`);
 
 --
--- Indices de la tabla `tblhistoriaclinica`
+-- Indexes for table `detalleformula`
 --
-ALTER TABLE `tblhistoriaclinica`
+ALTER TABLE `detalleformula`
+  ADD PRIMARY KEY (`iddetalleformula`,`formula_idFormula`,`medicamento_idMedicamento`),
+  ADD KEY `fk_detalleformula_formula1_idx` (`formula_idFormula`),
+  ADD KEY `fk_detalleformula_medicamento1_idx` (`medicamento_idMedicamento`);
+
+--
+-- Indexes for table `entrada`
+--
+ALTER TABLE `entrada`
+  ADD PRIMARY KEY (`idmovimientosMedicamentos`);
+
+--
+-- Indexes for table `formula`
+--
+ALTER TABLE `formula`
+  ADD PRIMARY KEY (`idFormula`,`medico_idtblMedico`,`paciente_idPaciente`),
+  ADD KEY `fk_formula_medico1_idx` (`medico_idtblMedico`),
+  ADD KEY `fk_formula_paciente1_idx` (`paciente_idPaciente`);
+
+--
+-- Indexes for table `historiaclinica`
+--
+ALTER TABLE `historiaclinica`
   ADD PRIMARY KEY (`idHistoriaClinica`),
-  ADD KEY `fk_tblHistoriaClinica_tblMedico1_idx` (`tblMedico_idMedico`),
-  ADD KEY `fk_tblHistoriaClinica_tblPaciente1_idx` (`tblPaciente_idPaciente`);
+  ADD KEY `fk_tblHistoriaClinica_tblMedico1_idx` (`idMedico`),
+  ADD KEY `fk_tblHistoriaClinica_tblPaciente1_idx` (`idPaciente`);
 
 --
--- Indices de la tabla `tblhistoriaclinica_has_tblformulamedica`
+-- Indexes for table `medicamento`
 --
-ALTER TABLE `tblhistoriaclinica_has_tblformulamedica`
-  ADD PRIMARY KEY (`tblHistoriaClinica_idHistoriaClinica`,`tblFormulaMedica_idFormula`),
-  ADD KEY `fk_tblHistoriaClinica_has_tblFormulaMedica_tblFormulaMedica_idx` (`tblFormulaMedica_idFormula`),
-  ADD KEY `fk_tblHistoriaClinica_has_tblFormulaMedica_tblHistoriaClini_idx` (`tblHistoriaClinica_idHistoriaClinica`);
+ALTER TABLE `medicamento`
+  ADD PRIMARY KEY (`idMedicamento`,`Entrada_idmovimientosMedicamentos`),
+  ADD KEY `fk_medicamento_Entrada1_idx` (`Entrada_idmovimientosMedicamentos`);
 
 --
--- Indices de la tabla `tblmedicamento`
+-- Indexes for table `medico`
 --
-ALTER TABLE `tblmedicamento`
-  ADD PRIMARY KEY (`idMedicamentos`),
-  ADD KEY `fk_tblMedicamento_tblFormulaMedica1_idx` (`tblFormulaMedica_idFormula`);
-
---
--- Indices de la tabla `tblmedico`
---
-ALTER TABLE `tblmedico`
+ALTER TABLE `medico`
   ADD PRIMARY KEY (`idMedico`),
   ADD UNIQUE KEY `idtblMedico_UNIQUE` (`idMedico`);
 
 --
--- Indices de la tabla `tblpaciente`
+-- Indexes for table `paciente`
 --
-ALTER TABLE `tblpaciente`
+ALTER TABLE `paciente`
   ADD PRIMARY KEY (`idPaciente`),
   ADD UNIQUE KEY `documentoPaciente_UNIQUE` (`documentoPaciente`),
   ADD UNIQUE KEY `emailPaciente_UNIQUE` (`emailPaciente`),
   ADD UNIQUE KEY `idtblPaciente_UNIQUE` (`idPaciente`);
 
 --
--- Indices de la tabla `tblrol`
+-- Indexes for table `rol`
 --
-ALTER TABLE `tblrol`
+ALTER TABLE `rol`
   ADD PRIMARY KEY (`idRol`);
 
 --
--- Indices de la tabla `tblusuario`
+-- Indexes for table `usuario`
 --
-ALTER TABLE `tblusuario`
+ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idUsuario`),
   ADD UNIQUE KEY `correoUsuario_UNIQUE` (`correoUsuario`),
-  ADD KEY `fk_tblUsuario_tblRol1_idx` (`tblRol_idRol`);
+  ADD KEY `fk_tblUsuario_tblRol1_idx` (`Rol_idRol`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `tblcitas`
+-- AUTO_INCREMENT for table `citas`
 --
-ALTER TABLE `tblcitas`
+ALTER TABLE `citas`
   MODIFY `idCitas` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `tblformulamedica`
+-- AUTO_INCREMENT for table `detalleentrada`
 --
-ALTER TABLE `tblformulamedica`
+ALTER TABLE `detalleentrada`
+  MODIFY `detalleEntrada` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `entrada`
+--
+ALTER TABLE `entrada`
+  MODIFY `idmovimientosMedicamentos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `formula`
+--
+ALTER TABLE `formula`
   MODIFY `idFormula` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `tblhistoriaclinica`
+-- AUTO_INCREMENT for table `historiaclinica`
 --
-ALTER TABLE `tblhistoriaclinica`
+ALTER TABLE `historiaclinica`
   MODIFY `idHistoriaClinica` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `tblmedicamento`
+-- AUTO_INCREMENT for table `medicamento`
 --
-ALTER TABLE `tblmedicamento`
-  MODIFY `idMedicamentos` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `medicamento`
+  MODIFY `idMedicamento` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `tblmedico`
+-- AUTO_INCREMENT for table `paciente`
 --
-ALTER TABLE `tblmedico`
-  MODIFY `idMedico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT de la tabla `tblpaciente`
---
-ALTER TABLE `tblpaciente`
+ALTER TABLE `paciente`
   MODIFY `idPaciente` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `tblrol`
---
-ALTER TABLE `tblrol`
-  MODIFY `idRol` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `tblusuario`
---
-ALTER TABLE `tblusuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `tblcitas`
+-- Constraints for table `citas`
 --
-ALTER TABLE `tblcitas`
-  ADD CONSTRAINT `fk_tblCitas_tblMedico1` FOREIGN KEY (`tblMedico_idMedico`) REFERENCES `tblmedico` (`idMedico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_tblCitas_tblPaciente1` FOREIGN KEY (`tblPaciente_idPaciente`) REFERENCES `tblpaciente` (`idPaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `citas`
+  ADD CONSTRAINT `fk_tblCitas_tblMedico1` FOREIGN KEY (`idMedico`) REFERENCES `medico` (`idMedico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_tblCitas_tblPaciente1` FOREIGN KEY (`idPaciente`) REFERENCES `paciente` (`idPaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `tblhistoriaclinica`
+-- Constraints for table `detalleentrada`
 --
-ALTER TABLE `tblhistoriaclinica`
-  ADD CONSTRAINT `fk_tblHistoriaClinica_tblMedico1` FOREIGN KEY (`tblMedico_idMedico`) REFERENCES `tblmedico` (`idMedico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_tblHistoriaClinica_tblPaciente1` FOREIGN KEY (`tblPaciente_idPaciente`) REFERENCES `tblpaciente` (`idPaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `detalleentrada`
+  ADD CONSTRAINT `fk_detalleEntrada_Entrada1` FOREIGN KEY (`Entrada_idmovimientosMedicamentos`) REFERENCES `entrada` (`idmovimientosMedicamentos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `tblhistoriaclinica_has_tblformulamedica`
+-- Constraints for table `detalleformula`
 --
-ALTER TABLE `tblhistoriaclinica_has_tblformulamedica`
-  ADD CONSTRAINT `fk_tblHistoriaClinica_has_tblFormulaMedica_tblFormulaMedica1` FOREIGN KEY (`tblFormulaMedica_idFormula`) REFERENCES `tblformulamedica` (`idFormula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_tblHistoriaClinica_has_tblFormulaMedica_tblHistoriaClinica1` FOREIGN KEY (`tblHistoriaClinica_idHistoriaClinica`) REFERENCES `tblhistoriaclinica` (`idHistoriaClinica`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `detalleformula`
+  ADD CONSTRAINT `fk_detalleformula_formula1` FOREIGN KEY (`formula_idFormula`) REFERENCES `formula` (`idFormula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalleformula_medicamento1` FOREIGN KEY (`medicamento_idMedicamento`) REFERENCES `medicamento` (`idMedicamento`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `tblmedicamento`
+-- Constraints for table `formula`
 --
-ALTER TABLE `tblmedicamento`
-  ADD CONSTRAINT `fk_tblMedicamento_tblFormulaMedica1` FOREIGN KEY (`tblFormulaMedica_idFormula`) REFERENCES `tblformulamedica` (`idFormula`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `formula`
+  ADD CONSTRAINT `fk_formula_medico1` FOREIGN KEY (`medico_idtblMedico`) REFERENCES `medico` (`idMedico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_formula_paciente1` FOREIGN KEY (`paciente_idPaciente`) REFERENCES `paciente` (`idPaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `tblusuario`
+-- Constraints for table `historiaclinica`
 --
-ALTER TABLE `tblusuario`
-  ADD CONSTRAINT `fk_tblUsuario_tblRol1` FOREIGN KEY (`tblRol_idRol`) REFERENCES `tblrol` (`idRol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `historiaclinica`
+  ADD CONSTRAINT `fk_tblHistoriaClinica_tblMedico1` FOREIGN KEY (`idMedico`) REFERENCES `medico` (`idMedico`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_tblHistoriaClinica_tblPaciente1` FOREIGN KEY (`idPaciente`) REFERENCES `paciente` (`idPaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `medicamento`
+--
+ALTER TABLE `medicamento`
+  ADD CONSTRAINT `fk_medicamento_Entrada1` FOREIGN KEY (`Entrada_idmovimientosMedicamentos`) REFERENCES `entrada` (`idmovimientosMedicamentos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_tblUsuario_tblRol1` FOREIGN KEY (`Rol_idRol`) REFERENCES `rol` (`idRol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
