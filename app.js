5c7e19bccccacaf14417af298df5bd0c6b816705
dranko1001 <franklinpenilla2001@gmail.com>
2026-02-27 08:24:25 -0500

    rutas corregitas, npm corriendo

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ import medicoRutas from "./src/routes/medico.routes.js";
 import medicamentoRutas from "./src/routes/medicamento.routes.js";
 import usuarioRutas from "./src/routes/usuario.routes.js";
 import rolRutas from "./src/routes/rol.routes.js";
+import pacienteRutas from "./src/routes/paciente.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 //  Las 4 rutas registradas correctamente
-app.use("/api", usuariosRutas);
 app.use("/api", medicoRutas);
 app.use("/api", medicamentoRutas);
 app.use("/api", pacienteRutas);
