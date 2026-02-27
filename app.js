8f7737802e1e8eb4e5ef9380d158531e6d7235a1
cesar <cesarrodas113@gmail.com>
2026-02-27 08:18:13 -0500

    se corrigen erroes de rutas en el modulo de rol

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import medicoRutas from "./src/routes/medico.routes.js";
 import pacienteRutas from "./src/routes/paciente.routes.js";
 import medicamentoRutas from "./src/routes/medicamento.routes.js";
 import usuarioRutas from "./src/routes/usuario.routes.js";
+import rolRutas from "./src/routes/rol.routes.js";
 
 //instanciamos la libreria en un objeto - app
 const app = express();
@@ -15,4 +16,5 @@ app.use("/api", medicoRutas);
 app.use("/api", medicamentoRutas);
 app.use("/api", pacienteRutas);
 app.use("/api", usuarioRutas);
+app.use("/api", rolRutas);
 export default app;
