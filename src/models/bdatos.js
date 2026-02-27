f96a0f334ddef5cccae3d33b19b64b903b87f11f
cesar <cesarrodas113@gmail.com>
2026-02-27 10:45:03 -0500

    se corrigen parametros de bdados.js y de .env

--- src/models/bdatos.js
+++ src/models/bdatos.js
@@ -1,25 +1,23 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 dotenv.config(); //carga las variables de entorno desde el archivo .env
-let cnx ="";
+let cnx = "";
 try {
-    cnx =mysql.createPool({
-        host:process.env.HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_BASE,
-        port:process.env.DB_PORT,
-        wififorconnection:true,
-        connectionlimit: 10,
-        queuelimit: 0
-    });
-    const conexion = await cnx.getConnection();
-    console.log("conexion exitosa a la base de datos");
-    conexion.release(); //CIERRA LA CONEXION DE PRUEBA
-    // console.log("conexion exitosa a la base de datos"); 
+  cnx = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_BASE,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+  const conexion = await cnx.getConnection();
+  console.log("conexion exitosa a la base de datos");
+  conexion.release(); //CIERRA LA CONEXION DE PRUEBA
+  // console.log("conexion exitosa a la base de datos");
+} catch (error) {
+  console.log(`error en la conexion a la base de datos ${error.message}`);
 }
-catch (error) {
-    console.log(`error en la conexion a la base de datos ${error.message}`);   
-};
-export const db=cnx;
-
+export const db = cnx;
